f30ff4b6a74eaf4eb1130e9143e22e8f1a9dd9a7
hsy <975892929@qq.com>
2022-01-10 23:56:18 +0800

    Location controller done.

---
+++ src/main/java/com/DB/course_work/Controller/LocController.java
@@ -0,0 +1,29 @@
+package com.DB.course_work.Controller;
+
+
+import com.DB.course_work.DAO.entities.Location;
+import com.DB.course_work.DAO.utils.JsonResult;
+import com.DB.course_work.Service.LocService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.DB.course_work.Controller.BasicController.OK;
+
+@RestController
+@RequestMapping("loc")
+public class LocController {
+
+    @Autowired
+    private LocService locService;
+
+    @RequestMapping("/getAllLoc")
+    /**
+     * Example: http://localhost:8080/loc/getAllLoc
+     */
+    public JsonResult<List<Location>> getAllLocInfo(){
+        return new JsonResult<>(OK, locService.findAllLocInfo());
+    }
+}
